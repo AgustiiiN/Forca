728ef4c54402a8af809086b0f27869c73261805d
AgustiiiN <63130287+AgustiiiN@users.noreply.github.com>
2024-01-23 22:49:42 -0300

    V2
    Adicionado o desenho da força

--- forca.cpp
+++ forca.cpp
@@ -1,19 +1,15 @@
 #include<iostream>
 #include<locale.h>
 
+void forca(int chances);
 
 int main() {
 	setlocale(LC_ALL, "");
 inicio:
 
 	char palavra[30], letra[1], secreta[30], restart;
-	int tam, i, chances, acertos;
-	bool acerto = false;
-
-	chances = 6;
-	tam = 0;
-	i = 0;
-	acertos = 0;
+	int tam{ 0 }, i{ 0 }, acertos{ 0 }, chances{ 6 };
+	bool acerto=false;
 
 	std::cout << "Digite a palavra secreta: ";
 	std::cin >> palavra;
@@ -23,28 +19,29 @@ inicio:
 		i++;
 		tam++;
 	}
-	
-	for (i = 0; i < 30; i++) {
+
+	for (i = 0; i < 30; ++i) {
 		secreta[i] = '-';
 	}
 
 	while ((chances > 0) && (acertos < tam)) {
-		std::cout << "Jogadas restantes: " << chances <<"\n\n";
-		std::cout << "Palavra secreta: ";
-		for (i = 0; i < tam; i++) {
+		std::cout << "Jogadas restantes: " << chances <<"\n";
+		forca(chances);
+		std::cout << "\nPalavra secreta: ";
+		for (i = 0; i < tam; ++i) {
 			std::cout << secreta[i];
 		}
 		std::cout << "\n\nDigite uma letra: ";
 		std::cin >> letra[0];
-		for (i = 0; i < tam; i++) {
+		for (i = 0; i < tam; ++i) {
 			if (palavra[i] == letra[0]){
 				acerto = true;
 				secreta[i] = palavra[i];
-				acertos++;
+				++acertos;
 				}
 		}
 		if (!acerto) {
-			chances--;
+			--chances;
 		}
 		acerto = false;
 		system("cls");
@@ -70,4 +67,31 @@ inicio:
 	}
 		
 	return 0;
+}
+
+void forca(int chances) {
+	switch (chances)
+	{
+	case 5:
+		std::cout << "_______\n |   |\n |   O\n | \n |\n |\n |\n==========\n"; //Cabeça
+		break;
+	case 4:
+		std::cout << "_______\n |   |\n |   O\n |  / \n |\n |\n |\n==========\n"; //Cabeça e braço esquerdo
+		break;
+	case 3:
+		std::cout << "_______\n |   |\n |   O\n |  / \\\n |\n |\n |\n==========\n"; //cabeça e ambos braços
+		break;
+	case 2:
+		std::cout << "_______\n |   |\n |   O\n |  / \\\n |   |\n |\n |\n==========\n"; //Cab, breços e Tronco
+		break;
+	case 1:
+		std::cout << "_______\n |   |\n |   O\n |  / \\\n |   |\n |  / \n |\n==========\n"; //Cab, breços, Tronco e perna esquerda
+		break;
+	case 0:
+		std::cout << "_______\n |   |\n |   O\n |  / \\\n |   |\n |  / \\\n |\n==========\n"; //Corpo completo
+		break;
+	default:
+		std::cout << "_______\n |   |\n |\n |\n |\n |\n |\n==========\n"; //Vazia
+		break;
+	}
 }
